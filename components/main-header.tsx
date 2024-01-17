411a08a4b0bff896f77af1093c19ff3869e1fd20
StephenKiiyuruNganga <steveknganga@gmail.com>
2024-01-17 13:51:52 +0300

    update navigation bar

--- components/main-header.tsx
+++ components/main-header.tsx
@@ -11,15 +11,59 @@ import {
   NavigationMenuViewport,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
-
+import { SheetTrigger, SheetContent, Sheet } from "@/components/ui/sheet"
 import Link from "next/link"
 import { ModeToggle } from "./mode-toggle"
 import { TypographyH3, TypographyH4 } from "./typography"
 import { Separator } from "@/components/ui/separator"
+import { Button } from "@/components/ui/button"
+import { type ComponentPropsWithoutRef } from "react"
 
-type NavItemProps = { link: string; label: string }
+type SvgProps = ComponentPropsWithoutRef<"svg">
 
-const NavItems: NavItemProps[] = [
+function MenuIcon(props: SvgProps) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <line x1="4" x2="20" y1="12" y2="12" />
+      <line x1="4" x2="20" y1="6" y2="6" />
+      <line x1="4" x2="20" y1="18" y2="18" />
+    </svg>
+  )
+}
+
+function MountainIcon(props: SvgProps) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
+    </svg>
+  )
+}
+
+type NavItem = { link: string; label: string }
+
+const NavItems: NavItem[] = [
   {
     link: "/meals",
     label: "Browse Meals",
@@ -30,37 +74,70 @@ const NavItems: NavItemProps[] = [
   },
 ]
 
-function NavItem({ link, label }: NavItemProps) {
-  return (
-    <NavigationMenuItem>
-      <Link href={link} legacyBehavior passHref>
-        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-          {label}
-        </NavigationMenuLink>
-      </Link>
-    </NavigationMenuItem>
-  )
-}
-
 export default function MainHeader() {
   return (
     <>
-      <header className="flex flex-row justify-between items-center px-10 py-3">
-        <div className="flex flex-row gap-10 items-center">
-          <Link href={"/"}>
-            <TypographyH3>😋 Tasty Trove</TypographyH3>
+      <header className="flex h-16  md:h-20 w-full shrink-0 items-center px-4 md:px-6 justify-between">
+        <div className="flex items-center">
+          <Link className="mr-6 flex items-center gap-2" href="/">
+            <MountainIcon className="h-6 w-6" />
+            <TypographyH4>Tasty Trove</TypographyH4>
+            <span className="sr-only">Tasty Trove</span>
           </Link>
 
-          <NavigationMenu>
+          <NavigationMenu className="hidden lg:flex">
             <NavigationMenuList>
               {NavItems.map((i) => {
-                return <NavItem key={i.label} label={i.label} link={i.link} />
+                return (
+                  <NavigationMenuItem key={i.label}>
+                    <Link href={i.link} legacyBehavior passHref>
+                      <NavigationMenuLink
+                        className={navigationMenuTriggerStyle()}
+                      >
+                        {i.label}
+                      </NavigationMenuLink>
+                    </Link>
+                  </NavigationMenuItem>
+                )
               })}
             </NavigationMenuList>
           </NavigationMenu>
         </div>
 
-        <ModeToggle />
+        <div className="flex items-center">
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button className="lg:hidden" size="icon" variant="ghost">
+                <MenuIcon className="h-6 w-6" />
+                <span className="sr-only">Toggle navigation menu</span>
+              </Button>
+            </SheetTrigger>
+
+            <SheetContent side="right">
+              <Link className="mr-6 hidden lg:flex" href="/">
+                <MountainIcon className="h-6 w-6" />
+                <TypographyH3>Tasty Trove</TypographyH3>
+                <span className="sr-only">Tasty Trove</span>
+              </Link>
+
+              <div className="grid gap-2 py-6">
+                {NavItems.map((i) => {
+                  return (
+                    <Link
+                      key={i.label}
+                      className="flex w-full items-center py-2 text-lg font-semibold"
+                      href={i.link}
+                    >
+                      {i.label}
+                    </Link>
+                  )
+                })}
+              </div>
+            </SheetContent>
+          </Sheet>
+
+          <ModeToggle />
+        </div>
       </header>
       <Separator />
     </>
